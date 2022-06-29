ae01ee45ca582cde9674e04e7ba7b635577cd4b9
eCrimeLabs <rand@ecrime.dk>
2022-06-29 20:59:21 +0200

    Version 0.1
    Release candidate

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
-# MISP-PurgeEvents
-Cleanup of older MISP events can require some work until now
+# MISP Purge old events
+
+A python script to perform cleanup of old or unwanted events, including Blacklisted Events
+
+Created by Dennis Rand [(@DennisRand)](https://twitter.com/dennisrand) - Company: eCrimeLabs ApS https://www.ecrimelabs.com
+
+## Detailed Description
+In various cases it would be useful to expire/purge older events, cleanup blocklist or delete events from a specific organization.
+
+The MISP-PurgeOldEvent tool can assist in various cleaning operations
+
+The tool supports exclusions on Organizations from purge, and also Feeds with fixed event will be excluded per default.  
+
+**Notice:**
+If a large set of events has to be purged it is highly recommended to disable/flush the correlation table prior, else we have seen MISP databases getting into unstable states, due to waits for cleanups in correlations.
+
+This can be achieved by logging in to MISP as a site admin, and go through "Administration" -> "Server Settings & Administration" -> "MISP Settings" -> Change "MISP.completely_disable_correlation" to True
+
+Validate that the correlations table has been clean by checking "Diagnostics" under "SQL database status".
+
+When the large task is completed remember to enable correlations again.
+
+**Warning:**
+- It is always recommended to perform a backup prior to deletion of data, and as minimum do a dryrun first to understand the events to be deleted.
+- Never run this on an MISP instance you do not own.
+
+## Benchmarks
+For a MISP instance with the below data volume (Correlations were removed prior to running the tool)
+- Events: 9.460 (From 2011 and until 2022)
+- Attributes: 1.424.251
+
+Execution time **12m11.239s**
+
+---
+
+## Dependencies
+It is recommended to create a virtual environment for the packages - **python 3.8 or higher** required.
+
+Example for creating the virtual environment, can be executed from within the "MISP-PurgeOldEvents" folder
+```
+# python3 -m venv venv
+```
+
+**Installing the requirements:**
+```
+./venv/bin/pip install -r requirements.txt
+```
+
+---
+## Config
+Before you execute the tool the first time a few tasks has to be completed.
+
+### config.py
+rename the "config.py.template" to "config.py" and add in you MISP data and the organization UUID and chunk size and exclusion organizations
+
+```
+misp_url='https://<MISP DOMAIN>'
+misp_key='<MISP KEY>'
+misp_verifycert=True
+
+# chunk_size is how many events to delete at a time.
+chunk_size = 200
+
+# Organization UUID's to exclude from deletion, typically you add your own organization.
+exclude_orgs = [
+     '1cb16c82-c808-4342-a874-60574c9c4df9',
+     '569b6c1f-bd1c-49c8-9244-0484bce2ab96'
+]
+```
+
+---
+
+## Usage
+
+**Help Menu:**
+ ```
+ $ venv/bin/python misp-purgeoldevents.py -h
+
+ eCrimeLabs MISP Purge Old Events tool
+ usage: misp-purgeoldevents.py [-h] [-f FIRST] [-l LAST] [-d] [-v] [-b] [-o ORGUUID] [--force]
+
+ optional arguments:
+   -h, --help            show this help message and exit
+   -f FIRST, --first FIRST
+                         Oldest events to purge - Format: YYYY-MM-DD (Default: 2022-06-26)
+   -l LAST, --last LAST  Newest events to purge - Format: YYYY-MM-DD (Default: 2022-06-27)
+   -d, --dryrun          Dryrun will not perform the deletion but output how many entries would be deleted.
+   -v, --verbose         Will output a list of the event UUID deleted, while running
+   -b, --blocklist       The purge will be done towards events listed in the BlockListed Events
+   -o ORGUUID, --orguuid ORGUUID
+                         Specify a specific organization UUID to perform purge on - Format: 123e4567-e89b-12d3-a456-426614174000
+   --force               The purge will performed, without asking for confirmation [WARNING - DELETION WILL BE DONE]
+ ```
+
+
+### Examples
+
+Delete all events between 2010-01-01 and 2022-12-12, excluding events created by Org UUID 569b6c1f-bd1c-49c8-9244-0484bce2ab96
+ ```
+ $ venv/bin/python misp-purgeoldevents.py -f 2010-01-01 -l 2022-12-12 -v --dryrun
+ eCrimeLabs MISP Purge Old Events tool
+  - Find all events for deletion between: 2010-01-01 and 2022-12-12
+  - 1 fixed events for exclusion from Feeds
+   - Searching events in MISP between 2010-01-01 and 2022-12-12
+   - 15 events exluded based on OrgC UUID's
+   - Excluded OrgC UUID(s):
+    + OrgC UUID: 569b6c1f-bd1c-49c8-9244-0484bce2ab96
+   - 9341 events identified and up for deletion, splitting into chunks of 200
+ ```
+
+Delete all events between 2010-01-01 and 2022-12-12, only events created by Organization UUID a40ea2c0-ff84-44ad-a936-7b1f5ab9725f
+```
+$ venv/bin/python misp-purgeoldevents.py -f 2010-01-01 -l 2022-12-12 -v -o a40ea2c0-ff84-44ad-a936-7b1f5ab9725f --dryrun
+eCrimeLabs MISP Purge Old Events tool
+ - Find events from organization a40ea2c0-ff84-44ad-a936-7b1f5ab9725f for deletion between: 2010-01-01 and 2022-12-12
+ - 1 fixed events for exclusion from Feeds
+  - Searching events in MISP between 2010-01-01 and 2022-12-12
+  - 0 events exluded based on OrgC UUID's
+  - Excluded OrgC UUID(s):
+   + OrgC UUID: 1cb16c82-c808-4342-a874-60574c9c4df9
+   + OrgC UUID: 569b6c1f-bd1c-49c8-9244-0484bce2ab96
+  - 1 events identified and up for deletion
+    - Result: 1 event(s) deleted, and 0 Failed
+  - Summarized Result: 1 events deleted, and 0 Failed
+ ------------------------------------------
+ - Purge Completed
+```
+
+In some cases you need to do tests where you create events and delete these and looping that task, so you want to remove blocklisted events between 2010-01-01 and 2022-12-12
+```
+$ venv/bin/python misp-purgeoldevents.py -f 2010-01-01 -l 2022-12-12 --blocklist --dryrun
+eCrimeLabs MISP Purge Old Events tool
+ - Find Blocklisted events for deletion between: 2010-01-01 and 2022-12-12
+ - 1 fixed events for exclusion from Feeds
+ - Searching and deleting blocklisted events in MISP based event date (NOTICE: This can take some time)
+ - Result: 4830 blocklisted events deleted, and 0 Failed
+ - Summarized Result: 0 events deleted, and 0 Failed
+------------------------------------------
+- Purge Completed
+```
---
+++ misp-purgeevents.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Created by Dennis Rand
+# eCrimeLabs ApS
+# https://www.ecrimelabs.com
+#
+
+import argparse
+import re
+import json
+import sys
+import arrow
+import pprint
+import datetime
+import time
+import signal
+import requests
+import uuid
+from tendo import singleton
+from pymisp import ExpandedPyMISP
+from pymisp import PyMISP
+from collections import defaultdict
+from config import misp_url, misp_key, misp_verifycert, exclude_orgs, chunk_size
+
+verbose = False
+dryrun = False
+blocklisted = False
+version = '0.1'
+
+def handler(signum, frame):
+    print ("")
+    res = input("Ctrl-c was pressed. Do you really want to exit? y/n ")
+    if res == 'y':
+        sys.exit(1)
+
+def is_valid_uuid(val):
+    try:
+        uuid.UUID(str(val))
+        return True
+    except ValueError:
+        return False
+
+def timestamp_today():
+    return(datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"))
+
+def timestamp_yesterday():
+    today = datetime.datetime.utcnow().date()
+    yesterday = today - datetime.timedelta(days=1)
+    return(str(yesterday))
+
+def timestamp():
+    return(datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S %Z - "))
+
+def valid_date(date_string):
+    try:
+        datetime.datetime.strptime(date_string, '%Y-%m-%d')
+        return(True)
+    except ValueError:
+        return(False)
+
+def get_active_lists_fixed_id(misp):
+    '''
+        Search for fixed event ID's added to Feeds.
+        Returns: List of MISP ID's
+    '''
+    global verbose
+    exclude_event_ids = []
+    response_lists = misp.feeds()
+    for list in response_lists:
+        if (int(list['Feed']['event_id']) > 0):
+            exclude_event_ids.append(list['Feed']['event_id'])
+    if (verbose):
+        print (" - " + str(len(exclude_event_ids)) + " fixed events for exclusion from Feeds")
+    return(exclude_event_ids)
+
+def search_and_delete_blocklist_events(misp, earliest, latest):
+    global dryrun, verbose, exclude_orgs
+    cntSuccess = 0
+    cntFailed = 0
+    print (" - Searching and deleting blocklisted events in MISP based event date (NOTICE: This can take some time)")
+    try:
+        response_lists = misp.event_blocklists()
+    except:
+        print (" - An Error occoured in search for events")
+        sys.exit(1)
+    if (verbose):
+        print (" - Deleting Blocklisted Events:")
+    formatted_earliest_date = time.strptime(earliest + ' 00:00:01', "%Y-%m-%d %H:%M:%S")
+    formatted_latest_date = time.strptime(latest + ' 23:59:59', "%Y-%m-%d %H:%M:%S")
+    for response in response_lists:
+            created_at = response['created']
+            event_uuid = response['event_uuid']
+            formatted_created_date = time.strptime(created_at, "%Y-%m-%d %H:%M:%S")
+            if ((formatted_created_date >= formatted_earliest_date) and (formatted_created_date <= formatted_latest_date)):
+                if (verbose):
+                    if (dryrun):
+                        print ("   + Blocklisted Event UUID: " + event_uuid)
+                    else:
+                        print ("   + Blocklisted Event UUID: " + event_uuid, end =" -> ")
+                if not (dryrun):
+                    result = misp.delete_event_blocklist(event_uuid)
+                    if (verbose):
+                        if (result['success']):
+                            print ("OK")
+                            cntSuccess = cntSuccess + 1
+                        else:
+                            print ("FAILED")
+                            cntFailed = cntFailed + 1
+                else:
+                    cntSuccess = cntSuccess + 1
+    return(cntSuccess,cntFailed)
+
+
+
+
+def search_misp_events(misp, time_list, exclude_event_ids, orguuid):
+    global dryrun, verbose, exclude_orgs
+    print ("  - Searching events in MISP between " + time_list[0] + " and " + time_list[1])
+    search_template = {
+        "published": "1",
+        "minimal": "1",
+    }
+    search_template['datefrom'] = time_list[0]
+    search_template['dateuntil'] = time_list[1]
+
+    events = []
+    org_counter = 0
+    try:
+        response_lists = misp.direct_call('/events/index', search_template)
+    except:
+        print (" - An Error occoured in search for events")
+        sys.exit(1)
+    for response in response_lists:
+        event_uuid = response['uuid']
+        orgc_uuid = response['orgc_uuid']
+        event_id = response['id']
+        if not orgc_uuid in exclude_orgs:
+            if not event_id in exclude_event_ids:
+                if(is_valid_uuid(orguuid)):
+                    if (orgc_uuid == orguuid):
+                        events.append(int(event_id))
+                else:
+                    events.append(int(event_id))
+        else:
+            org_counter = org_counter + 1
+    if (verbose or dryrun):
+        if(is_valid_uuid(orguuid)):
+            print ("  - 0 events exluded based on OrgC UUID's")
+        else:
+            print ("  - " + str(org_counter) + " events exluded based on OrgC UUID's")
+        if (verbose):
+            print ("  - Excluded OrgC UUID(s):")
+            for exclude_org in exclude_orgs:
+                print ("   + OrgC UUID: " + exclude_org)
+    return(events)
+
+def delete_misp_events(misp, event_db):
+    global dryrun, verbose, misp_url, misp_key, chunk_size
+    cntSuccess = 0
+    cntFailed = 0
+    bulk = {}
+    if (len(event_db) > 0):
+        bulk['id'] = event_db
+        app_json = json.dumps(bulk)
+        url = misp_url + '/events/delete'
+        try:
+            x = requests.post(url, data=app_json, headers={"Content-Type":"application/json", "Accept":"application/json", "Authorization": misp_key }, timeout=60)
+            cntSuccess = len(event_db)
+        except:
+            cntFailed = len(event_db)
+    return(cntSuccess,cntFailed)
+
+def perform_task(first, last, blocklisted, force, orguuid):
+    global dryrun, verbose, split_freq, chunk_size
+    cntSuccessSum = 0
+    cntFailedSum = 0
+    me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
+    delete_data = False
+    if (force == False and dryrun == False):
+        answer = input(" - Continue data deletion in MISP (answer: YES to continue)?")
+        if answer.upper() in ["Y", "YES"]:
+            delete_data = True
+        else:
+            delete_data = False
+            print (" - Data purge has exited use '--dryrun' for test or '--force' to run automated")
+            sys.exit(0)
+    else:
+        delete_data = True
+
+    if (delete_data):
+        signal.signal(signal.SIGINT, handler)
+        misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert)
+        exclude_event_ids = get_active_lists_fixed_id(misp)
+
+        formatted_earliest_date = time.strptime(first, "%Y-%m-%d")
+        formatted_latest_date = time.strptime(last, "%Y-%m-%d")
+        if (formatted_latest_date < formatted_earliest_date):
+            print (" - Failed since first seen (" + first + ") is after last seen (" + last + ")")
+            sys.exit(1)
+        if (blocklisted):
+            cntSuccess,cntFailed = search_and_delete_blocklist_events(misp, first, last)
+            print (" - Result: " + str(cntSuccess) + " blocklisted events deleted, and " + str(cntFailed) + " Failed")
+        else:
+            time_list = [str(first), str(last)]
+            event_db = search_misp_events(misp, time_list, exclude_event_ids, orguuid)
+            if (len(event_db) < chunk_size):
+                print("  - " + str(len(event_db)) + " events identified and up for deletion")
+            else:
+                print("  - " + str(len(event_db)) + " events identified and up for deletion, splitting into chunks of " + str(chunk_size))
+            if (dryrun):
+                    print ("------------------------------------------")
+                    print ("- Simulated Purge Completed")
+                    sys.exit(0)
+            if (len(event_db) > 0):
+                chunked_list = list()
+                loop_sleep = 0
+                for i in range(0, len(event_db), chunk_size):
+                    chunked_list.append(event_db[i:i+chunk_size])
+
+                for event_db in chunked_list:
+                    failed_attempts = 0
+                    cntSuccess, cntFailed = delete_misp_events(misp, event_db)
+                    cntSuccessSum = cntSuccessSum + cntSuccess
+                    cntFailedSum = cntFailedSum + cntFailed
+                    if(verbose):
+                        print ("    - Result: " + str(cntSuccess) + " event(s) deleted, and " + str(cntFailed) + " Failed")
+                    loop_sleep = loop_sleep + 1
+                    if (cntFailed >= 1):
+                        loop_sleep = 0
+                        failed_attempts = failed_attempts + 1
+                        if (force == False):
+                            print ("    - Sleeping 360 seconds - To give database time to recover and cleanup - Failed attempts")
+                            time.sleep(360)
+                    elif (loop_sleep == 10):
+                        loop_sleep = 0
+                        if (force == False):
+                            print ("    - Sleeping 120 seconds - To give database time to recover and cleanup - when more than 500+ events is to be deleted")
+                            time.sleep(120)
+                    elif (failed_attempts > 3):
+                        print ("- Multiple failed concurrent attempts... Exiting")
+                        sys.exit(1)
+                    else:
+                        failed_attempts = 0
+                        pass
+        print ("  - Summarized Result: " + str(cntSuccessSum) + " events deleted, and " + str(cntFailedSum) + " Failed")
+        print (" ------------------------------------------")
+
+def main():
+    global dryrun, verbose, blocklisted
+
+    print ("eCrimeLabs MISP Purge Events tool v." + str(version))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--first", type=str, default=timestamp_yesterday(), help="Oldest events to purge - Format: YYYY-MM-DD (Default: " + timestamp_yesterday() + ")")
+    parser.add_argument("-l", "--last", type=str, default=timestamp_today(), help="Newest events to purge - Format: YYYY-MM-DD (Default: " + timestamp_today() + ")")
+    parser.add_argument("-d", "--dryrun", default=False, action='store_true', help="Dryrun will not perform the deletion but output how many entries would be deleted.")
+    parser.add_argument("-v", "--verbose", default=False, action='store_true', help="Will output a list of the event UUID deleted, while running")
+    parser.add_argument("-b", "--blocklist", default=False, action='store_true', help="The purge will be done towards events listed in the BlockListed Events")
+    parser.add_argument("-o", "--orguuid", type=str, help="Specify a specific organization UUID to perform purge on - Format: 123e4567-e89b-12d3-a456-426614174000")
+    parser.add_argument("--force", default=False, action='store_true', help="The purge will performed, without asking for confirmation [WARNING - DELETION WILL BE DONE]")
+
+    args = parser.parse_args()
+    verbose = args.verbose
+    dryrun = args.dryrun
+    blocklisted = args.blocklist
+    force = args.force
+    orguuid = ""
+
+    if not (valid_date(args.first)):
+        print(" - First seen date seen date (" + args.first + ") has incorrect date string format. It should be YYYY-MM-DD")
+        sys.exit(1)
+    elif not (valid_date(args.last)):
+        print(" - Last seen date seen date (" + args.last + ") has incorrect date string format. It should be YYYY-MM-DD")
+        sys.exit(1)
+
+    if (dryrun):
+        print (" - Running in dryrun mode (NO DATA WILL BE DELETED)")
+    if (blocklisted):
+        print(" - Find Blocklisted events for deletion between: " + args.first + " and " + args.last)
+    else:
+        if args.orguuid is not None:
+            if (is_valid_uuid(args.orguuid)):
+                orguuid = args.orguuid
+                print(" - Find events from organization " + orguuid + " for deletion between: " + args.first + " and " + args.last)
+            else:
+                print ("ERROR: Invalid UUID(" + str(args.orguuid) + ") format, exiting")
+                sys.exit(1)
+        else:
+            print(" - Find all events for deletion between: " + args.first + " and " + args.last)
+
+    perform_task(args.first, args.last, blocklisted, force, orguuid)
+    if (dryrun):
+        print (" - Simulated Purge Completed")
+    else:
+        print (" - Purge Completed")
+
+if __name__ == '__main__':
+    main()
